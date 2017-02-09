7acd27767d7be81b336efa6f5de5940fc1993116
DavidFrisch <dmf463@nyu.edu>
2017-02-09 17:25:51 -0500

    level is set up, no idea what to do from here

--- Codelab1_Week3HW/Assets/Script/GameManagerScript.cs
+++ Codelab1_Week3HW/Assets/Script/GameManagerScript.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour {
+
+    private int score;
+    public int Score
+    {
+        get
+        {
+            Debug.Log("Score is Being Accessed From Here");
+            return score;
+        }
+        set
+        {
+            score = value;
+            Debug.Log("Score was set to: " + score);
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
---
+++ Codelab1_Week3HW/Assets/Script/TrashBinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Trash")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
