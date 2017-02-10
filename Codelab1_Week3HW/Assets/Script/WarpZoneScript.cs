7ade4333054530fc9f63b8d01a239b684da0cfbe
DavidFrisch <dmf463@nyu.edu>
2017-02-09 20:10:54 -0500

    Added in scene switch

---
+++ Codelab1_Week3HW/Assets/Script/WarpZoneScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WarpZoneScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        if (other.gameObject.tag == "Player")
+        {
+            if (currentScene.name == "Scene1")
+            {
+                SceneManager.LoadScene("AlternateDimension");
+            }
+            if (currentScene.name == "AlternateDimension")
+            {
+                SceneManager.LoadScene("Scene1");
+            }
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
